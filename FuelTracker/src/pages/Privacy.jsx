e3a787f656dd432778294f2abee1c81aa6f5558c
Suraj Ghosi <suraj.ghosi@dataart.com>
2025-10-15 18:34:57 +0530

    Go to Dashboard

--- FuelTracker/src/pages/Privacy.jsx
+++ FuelTracker/src/pages/Privacy.jsx
@@ -115,7 +115,7 @@ export default function Privacy() {
           </div>
 
           <Link
-            to="/"
+            to="/dashboard"
             className="rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700"
           >
             Go to Dashboard
