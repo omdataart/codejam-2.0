99595a85599fe524ac2810d0e160d0465f8a0945
Suraj Ghosi <suraj.ghosi@dataart.com>
2025-10-15 18:05:39 +0530

    remove

--- FuelTracker/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    // autoprefixer is optional; you can remove it if you want
-    // autoprefixer: {},
-  },
-};
